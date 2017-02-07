7a9a952beb9874ec03ce1b4d30fb3588c38a2d63
dan.supriti <dan.supriti2010@gmail.com>
2017-02-07 19:38:34 +0530

    added code for builder pattern

---
+++ src/com/sup/practice/builder/TestBuilderPattern.java
@@ -0,0 +1,11 @@
+package com.sup.practice.builder;
+
+/**
+ * Created by ACER on 07-02-2017.
+ */
+public class TestBuilderPattern {
+    public static void main(String args[]){
+        User user = new UserBuilder().firstName("Firstname").lastName("Lastname").age(12).build();
+        System.out.print(user.toString());
+    }
+}
