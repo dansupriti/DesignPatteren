c71f17713fd87ffd2780a16f7c79d9b6168b1ea2
dan.supriti <dan.supriti2010@gmail.com>
2017-02-08 17:09:39 +0530

    added prototype design pattern

---
+++ src/com/sup/practice/prototype/Rectangle.java
@@ -0,0 +1,13 @@
+package com.sup.practice.prototype;
+
+/**
+ * Created by ACER on 08-02-2017.
+ */
+public class Rectangle extends Shape {
+    public Rectangle(){
+        type = "Rectangle";
+    }
+    public void draw(){
+        System.out.println("Inside Rectangle");
+    }
+}
