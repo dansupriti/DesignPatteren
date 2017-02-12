e593fa17b8d319501c823a9acc86d7097ca62cfc
dan.supriti <dan.supriti2010@gmail.com>
2017-02-12 12:43:02 +0530

    Added Facade design pattern

---
+++ src/com/sup/practice/creational/structural/adapter/facade/Rectangle.java
@@ -0,0 +1,11 @@
+package com.sup.practice.creational.structural.adapter.facade;
+
+/**
+ * Created by ACER on 11-02-2017.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
---
+++ src/com/sup/practice/creational/structural/adapter/facade/Square.java
@@ -0,0 +1,11 @@
+package com.sup.practice.creational.structural.adapter.facade;
+
+/**
+ * Created by ACER on 11-02-2017.
+ */
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Square");
+    }
+}
